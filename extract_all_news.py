a8b14b922f05b57f985eb4a731f2d907925801c5
ShuhongWangWM <swang32@email.wm.edu>
2020-09-12 16:30:12 +0800

    fixed some bug

--- extract_all_news.py
+++ extract_all_news.py
@@ -6,17 +6,17 @@ import csv
 
 def extract_news():
     # build all newspaper source
-    bbc_paper = newspaper.build('https://www.bbc.com')
-    cnbc_paper = newspaper.build('https://www.cnbc.com/')
-    cnn_paper = newspaper.build('https://edition.cnn.com')
-    forbes_paper = newspaper.build('https://www.forbes.com')
-    fox_paper = newspaper.build('https://www.foxnews.com')
-    guardian_paper = newspaper.build('https://www.theguardian.com')
-    nbc_paper = newspaper.build('https://www.nbc.com')
-    npr_paper = newspaper.build('https://www.npr.org')
-    nytimes_paper = newspaper.build('https://www.nytimes.com')
-    wsj_paper = newspaper.build('https://www.wsj.com')
-    yahoo_paper = newspaper.build('https://www.yahoo.com')
+    bbc_paper = newspaper.build('https://www.bbc.com', memorize_articles=False)
+    cnbc_paper = newspaper.build('https://www.cnbc.com/', memorize_articles=False)
+    cnn_paper = newspaper.build('https://edition.cnn.com', memorize_articles=False)
+    forbes_paper = newspaper.build('https://www.forbes.com', memorize_articles=False)
+    fox_paper = newspaper.build('https://www.foxnews.com', memorize_articles=False)
+    guardian_paper = newspaper.build('https://www.theguardian.com', memorize_articles=False)
+    nbc_paper = newspaper.build('https://www.nbc.com', memorize_articles=False)
+    npr_paper = newspaper.build('https://www.npr.org', memorize_articles=False)
+    nytimes_paper = newspaper.build('https://www.nytimes.com', memorize_articles=False)
+    wsj_paper = newspaper.build('https://www.wsj.com', memorize_articles=False)
+    yahoo_paper = newspaper.build('https://www.yahoo.com', memorize_articles=False)
 
     papers = [bbc_paper, cnbc_paper, cnn_paper, forbes_paper, fox_paper, guardian_paper, nbc_paper, npr_paper, nytimes_paper, wsj_paper, yahoo_paper]
 
@@ -29,15 +29,18 @@ def extract_news():
 
     for paper in papers:
         for article in paper.articles:
-            article.download()
+            try:
+                article.download()
+            except newspaper.article.ArticleException:
+                print("Failed to scrape some websites.")
             article.parse()
 
-            # if article.publish_date != None:
-            #     # check if the article is new
-            #     now = datetime.datetime.now(timezone.est)
-            #     time_elapsed = now - article.publish_date
-            #     if time_elapsed.total_seconds() > 172800:
-            #         continue
+            if article.publish_date != None:
+                # check if the article is new
+                now = datetime.datetime.now()
+                time_elapsed = now - article.publish_date
+                if time_elapsed.total_seconds() > 172800:
+                    continue
 
             # store the information of this article
             res = []
