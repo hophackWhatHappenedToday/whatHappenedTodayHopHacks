41102ddf2fed0cef740af070d2495d6b61c3210a
Yuqi Zhang <59454965+YuqiZ2020@users.noreply.github.com>
2020-09-12 22:52:54 +0800

    change_color_scale

--- cloud.js
+++ cloud.js
@@ -2,11 +2,11 @@ anychart.onDocumentReady(function () {
 
     // create data
     var data = [
-        {x: "COVID-19", value: 80, category: "Medicine"},
-        {x: "Coronavirus", value: 56, category: "Medicine"},
-        {x: "Brexit", value: 44, category: "Politics"},
-        {x: "Premier League ", value: 40, category: "Sports"},
-        {x: "Stevie Lee", value: 36, category: "People"},
+        {x: "COVID-19", value: 80/*, category: "Medicine"*/},
+        {x: "Coronavirus", value: 56/*, category: "Medicine"*/},
+        {x: "Brexit", value: 44/*, category: "Politics"*/},
+        {x: "Premier League ", value: 40/*, category: "Sports"*/},
+        {x: "Stevie Lee", value: 36/*, category: "People"*/},
     ];
 
     // create a chart and set the data
@@ -27,12 +27,14 @@ anychart.onDocumentReady(function () {
     });
 
     // create and configure a color scale.
-var customColorScale = anychart.scales.ordinalColor();
-customColorScale.colors(["#68b0ab", "#8fc0a9", "#c8d5b9", "#Faf3dd"]);
+    var customColorScale = anychart.scales.linearColor();;
+    customColorScale.colors(["#7bd5f5", "#5e72eb", "#120c6e"]);
 
-// set the color scale as the color scale of the chart
-chart.colorScale(customColorScale);
+    // set the color scale as the color scale of the chart
+    chart.colorScale(customColorScale);
 
-// add a color range
-chart.colorRange().enabled(true);
+    // add a color range
+    chart.colorRange().enabled(true);
+
+    chart.normal().fontFamily("Times New Roman");
 });
