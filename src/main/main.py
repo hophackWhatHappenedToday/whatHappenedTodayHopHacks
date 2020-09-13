6153792d06d8c8fc17bb19b2bf7a09b1d64e7cda
tottiliyt <58280109+tottiliyt@users.noreply.github.com>
2020-09-13 11:53:01 +0800

    respage

--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import os
 
-app = Flask(__name__)
+template_dir = os.path.abspath('../../templates')
+app = Flask(__name__, template_folder=template_dir, static_folder='../../static')
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -12,8 +14,18 @@ def main():
         return render_template('mainPage.html', message="GET")
     else:
         print("POST")
-
-    return render_template('resultPage.html', RESULT=)
+        source = request.form.getlist('newssource')
+        sort = request.form['sort']
+        time = request.form['time']
+        range = request.form['rangeInput']
+        categories = request.form.getlist('categories')
+        print(source)
+        print(sort)
+        print(time)
+        print(range)
+        print(categories)
+        data = filter(source, sort, time, range, categories)
+    return render_template('resultPage.html', data_dict=data)
 
 
 if __name__ == "__main__":
